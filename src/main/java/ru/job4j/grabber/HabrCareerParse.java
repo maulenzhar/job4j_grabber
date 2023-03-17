4fa2729f3ea8312229a444e710510448472bf43c
mzhar <life_thug@mail.ru>
2023-03-17 13:39:57 +0600

    Загрузка деталей поста.

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -26,8 +26,27 @@ public class HabrCareerParse {
                 String date = row.select(".vacancy-card__date").first()
                         .child(0).attr("datetime");
                 String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                String description = retrieveDescription(link);
                 System.out.printf("%s %s %s%n", vacancyName, link, new HabrCareerDateTimeParser().parse(date));
+                System.out.println(description);
             });
         }
+    }
+
+    private static String retrieveDescription(String link) {
+        StringBuilder description = new StringBuilder();
+        try {
+            Connection connection = Jsoup.connect(link);
+            Document document = connection.get();
+            Elements rows = document.select(".basic-section--appearance-vacancy-description");
+            rows.forEach(row -> {
+                Element descriptionElement = row.select(".faded-content__container").first();
+                description.append(descriptionElement.text());
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return description.toString();
     }
 }
